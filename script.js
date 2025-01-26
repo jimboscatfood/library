8953483fd42ac1468dca9b3a0b6d86e95af1d819
jimboscatfood <149596389+jimboscatfood@users.noreply.github.com>
2025-01-26 21:32:31 +0800

    Added event listener for submit button
    The data entered by users will be read when submit button is clicked

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Book (title,author,pages,read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read? "read":"not read yet";
+    this.read = read? "Read":"Not read yet";
     this.info = function () {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;         
     } 
@@ -26,19 +26,24 @@ function addBookToLibrary(title,author,pages,read) {
 }
 
 //trial 
-addBookToLibrary("title1","author1",1,0);
-addBookToLibrary("title2","'author2'",2,1);
-addBookToLibrary("title3","author3",3,0);
-addBookToLibrary("title4","author4",4,1);
-addBookToLibrary("title5","author5",5,0);
-console.table(myLibrary);
+// addBookToLibrary("title1","author1",1,0);
+// addBookToLibrary("title2","author2",2,1);
+// addBookToLibrary("title3","author3",3,0);
+// addBookToLibrary("title4","author4",4,1);
+// addBookToLibrary("title5","author5",5,0);
+// console.table(myLibrary);
 
 const library = document.querySelector('.library');
 
 
 function displayBook() {
+    while (library.hasChildNodes()) {
+    library.removeChild(library.firstChild);
+    }
     for (let i = 0; i < myLibrary.length; i++) {
         const card = document.createElement("div");
+        card.setAttribute("id",`${i}`);
+        card.setAttribute("class", "card");
         const book = myLibrary[i];
         library.appendChild(card);
 
@@ -60,18 +65,32 @@ function displayBook() {
     }
 }
 
-displayBook();
-
+// displayBook();
+//make reference to new book button and the dialog element
 const newBook = document.getElementById("newBook");
 const inputWindow = document.getElementById("addBook");
-
+//make reference to dialog buttons
 const cancelBtn = document.getElementById("cancel");
+const submitBtn = document.getElementById("submit");
 
 newBook.addEventListener("click",() => {
     inputWindow.showModal();  
 })
 
 cancelBtn.addEventListener("click", () => {
+    inputWindow.close();
+})
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    //make reference to input entries
+    const title = inputWindow.querySelector("#title").value;
+    const author = inputWindow.querySelector("#author").value;
+    const pages = parseInt(inputWindow.querySelector("#pages").value);
+    const read = inputWindow.querySelector("#read").value === "Read"?true:false;
+
+    addBookToLibrary(title, author, pages, read);
+    displayBook();
     inputWindow.close();
 })
 
